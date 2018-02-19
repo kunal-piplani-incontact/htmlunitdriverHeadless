76e6a97eec66bf934cc0ea0af2e733b59af3761e
kunal piplani <kunal.piplani@nice.com>
2018-02-19 15:53:22 +0530

    Update ClassTest.java

--- src/test/java/ClassTest.java
+++ src/test/java/ClassTest.java
@@ -1,7 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -10,27 +9,23 @@ import org.testng.annotations.Test;
  */
 public class ClassTest {
     // Creating a new instance of the HTML unit driver
+
+    // running simple test case with headless browser
     @Test
-    public static void test() {
-        WebDriver driver = new HtmlUnitDriver();
+    public static void test() throws InterruptedException {
 
         // Navigate to Google
-        driver.get("http://www.google.com");
-
-        // Locate the searchbox using its name
-        WebElement element = driver.findElement(By.name("q"));
-
-        // Enter a search query
-        element.sendKeys("Guru99");
-
-        // Submit the query. Webdriver searches for the form using the text input element automatically
-        // No need to locate/find the submit button
-        element.submit();
-
-        // This code will print the page title
-        System.out.println("Page title is: " + driver.getTitle());
-        Assert.assertEquals("Guru99 - Google Search","Guru99 - Google Search");
-        driver.quit();
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.gmail.com");
+        driver.findElement(By.id("Email")).sendKeys("Enter user name");
+        driver.findElement(By.id("next")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.id("Passwd")).sendKeys("Enter Password");
+        driver.findElement(By.id("signIn")).click();
+        Thread.sleep(2000);
+        String title_Of_Page = driver.getTitle();
+        Assert.assertEquals(driver.getTitle(), title_Of_Page);
+        System.out.println("Page title matched");
     }
 }
 
